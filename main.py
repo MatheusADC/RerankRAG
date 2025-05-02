258f34de4d8b9561c3175efb3dbd4d2068ca0071
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-05-02 17:01:55 -0300

    Add: Libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
+from langchain_cohere import CohereRerank
