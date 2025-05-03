d7efdd000024ec9d3bde6bdb8fb72a44dcc11246
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-05-03 16:20:01 -0300

    Add: Load the PDF

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ os.environ['OPENAI_API_KEY'] = "your openai api key"
 
 embeddings_model = OpenAIEmbeddings(model='text-embedding-3-small')
 llm = ChatOpenAI(model_name='gpt-3.5-turbo', max_tokens=300)
+
+pdf_link = 'your pdf link'
+loader = PyPDFLoader(pdf_link, extract_images=False)
+pages = loader.load_and_split()
